cecc559a4608d1448284658a6e81987dab3fb567
acs0209 <annjae4676@naver.com>
2023-08-25 11:18:04 +0900

    [feat] - initializing

---
+++ daedongje/src/main/java/backend/daedongje/DaedongjeApplication.java
@@ -0,0 +1,13 @@
+package backend.daedongje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaedongjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaedongjeApplication.class, args);
+	}
+
+}
