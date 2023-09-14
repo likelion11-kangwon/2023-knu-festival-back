3ac5a10b89afd76a8bee0dd221d9914d79ef56cf
acs0209 <annjae4676@naver.com>
2023-09-14 14:21:00 +0900

    [fit] - 공지사항 반환 데이터 수정

--- daedongje/src/main/java/backend/daedongje/web/dto/NoticeListResponseDto.java
+++ daedongje/src/main/java/backend/daedongje/web/dto/NoticeListResponseDto.java
@@ -4,17 +4,23 @@ import backend.daedongje.domain.Notice;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 
 @Getter
 @Setter
 public class NoticeListResponseDto {
 
-    private Long id;
+    private Long noticeId;
     private String title;
+    private String content;
+    private LocalDateTime createDate;
 
     //Entity -> Dto
     public NoticeListResponseDto(Notice notice){
+        this.noticeId = notice.getId();
         this.title = notice.getTitle();
-        this.id = notice.getId();
+        this.content = notice.getContent();
+        this.createDate = notice.getCreatedDate();
     }
 }
--- daedongje/src/main/java/backend/daedongje/web/dto/NoticeResponseDto.java
+++ daedongje/src/main/java/backend/daedongje/web/dto/NoticeResponseDto.java
@@ -4,6 +4,8 @@ import backend.daedongje.domain.Notice;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class NoticeResponseDto {
@@ -11,12 +13,14 @@ public class NoticeResponseDto {
     private Long noticeId;
     private String title;
     private String content;
+    private LocalDateTime createDate;
 
     //Entity -> Dto
     public NoticeResponseDto(Notice notice){
         this.noticeId = notice.getId();
         this.title = notice.getTitle();
         this.content = notice.getContent();
+        this.createDate = notice.getCreatedDate();
     }
 
 }
