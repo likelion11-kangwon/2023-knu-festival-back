30af0710d70623fa827bc201e3ea437627ffc0c4
acs0209 <annjae4676@naver.com>
2023-09-15 01:38:28 +0900

    [fit] - 테스트 코드 주석 처리

--- daedongje/src/test/java/backend/daedongje/DaedongjeApplicationTests.java
+++ daedongje/src/test/java/backend/daedongje/DaedongjeApplicationTests.java
@@ -1,13 +1,13 @@
-package backend.daedongje;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DaedongjeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package backend.daedongje;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DaedongjeApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
