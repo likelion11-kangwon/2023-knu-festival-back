5645b83b8adf6b21796ac8011d7432287b731da0
acs0209 <annjae4676@naver.com>
2023-09-15 11:36:48 +0900

    [fit] - CORS 설정

--- daedongje/src/main/java/backend/daedongje/config/WebMvcConfig.java
+++ daedongje/src/main/java/backend/daedongje/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("/**")    //외부에서 들어오는 모둔 url 을 허용
+                .allowedOrigins("*") //외부에서 들어오는 모둔 url 을 허용
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
                 .allowedHeaders("*")    //허용되는 헤더
                 .allowCredentials(true);    //자격증명 허용
