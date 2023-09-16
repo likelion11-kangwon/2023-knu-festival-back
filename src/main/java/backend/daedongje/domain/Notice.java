3fa898db574036bcbdf6b8f0b5e758c4985a3372
acs0209 <annjae4676@naver.com>
2023-09-17 00:45:38 +0900

    [fit] - 공지사항 내용 길이 제한 수정

--- src/main/java/backend/daedongje/domain/Notice.java
+++ src/main/java/backend/daedongje/domain/Notice.java
@@ -24,7 +24,7 @@ public class Notice {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length=1000)
     private String content;
 
     private boolean delCheck;
